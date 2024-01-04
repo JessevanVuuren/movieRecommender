eaf1ab2ac58fb85a97e9d4073ffa63d32082857f
Jesse van Vuuren <32089540+JessevanVuuren@users.noreply.github.com>
2024-01-04 22:54:41 +0100

    nice animation, before modal

--- components/movieTinder.tsx
+++ components/movieTinder.tsx
@@ -1,12 +1,21 @@
-import { StyleSheet, View, Image, Dimensions, GestureResponderEvent, Animated } from "react-native";
+import { StyleSheet, View, Image, Dimensions, GestureResponderEvent, Animated, Text } from "react-native";
 import React, { useEffect, useRef, useState } from "react"
-import { baseUrl500, makeURL } from "../src/helper";
+import { baseUrl500, getDate, makeURL } from "../src/helper";
+
 import { FontText } from "../components/fontText"
 import Colors from "../src/style"
+import { Movie } from "../models/movie";
 
 const WIDTH = Dimensions.get("window").width
 const ANIM_DURATION = 500
-const CARD_STACK = 30
+
+const animValues = [
+  { position: 10, opacity: .9, width: 1 },
+  { position: 20, opacity: .7, width: .9 },
+  { position: 30, opacity: .5, width: .8 },
+  { position: 40, opacity: .3, width: .7 },
+  { position: 50, opacity: .0, width: .6 }
+]
 
 interface MovieTinderProps {
 
@@ -71,7 +80,7 @@ const Card = (props: any) => {
       }
       if (likeMovie == "left") {
         startAnim(-600, -90, 0, resetCard)
-      } 
+      }
     } else {
       startAnim(0, 0, 0)
     }
@@ -80,7 +89,6 @@ const Card = (props: any) => {
   return (
     <View style={styles.card}>
       <Animated.View style={{ transform: [{ translateX: animPos }, { rotate: rotateImg }], }}>
-
         <Image style={styles.cardImg} source={{ uri: baseUrl500 + props.movie.poster_path }} />
         <Animated.View style={{ position: "absolute", height: "100%", width: "90%", backgroundColor: "#00D68F", borderRadius: 25, opacity: animOpacityGreen }} />
         <Animated.View style={{ position: "absolute", height: "100%", width: "90%", backgroundColor: "#d93b3b", borderRadius: 25, opacity: animOpacityRed }} />
@@ -89,6 +97,30 @@ const Card = (props: any) => {
       <View onTouchMove={touchMove} onTouchStart={touchStart} onTouchEnd={touchEnd} style={styles.moveCard}></View>
     </View>
   )
+}
+
+const ExtraInfo = (props: any) => {
+  const movie: Movie = props.movie
+
+
+  return (
+    <View style={styles.extraInfoContainer}>
+
+      <View style={styles.infoHolder}>
+        <View style={styles.infoRow}>
+          <View style={styles.info}>
+            <FontText fontSize={20} font={"Roboto-Bold"}>{getDate(movie.release_date)}</FontText>
+          </View>
+          <View style={[styles.info, styles.movieRating]}>
+            <Image source={require("../assets/star-symbol.png")} style={[styles.topStar, { height: 15.5, width: 16 }]} />
+            <Text style={styles.voteAverage}>{Math.round(movie.vote_average * 100) / 100}</Text>
+          </View>
+        </View>
+      </View>
+
+    </View>
+  )
+
 }
 
 const getMovies = async (page: number) => {
@@ -103,86 +135,78 @@ const MovieTinder: React.FC<MovieTinderProps> = props => {
   const [pageNumber, setPageNumber] = useState<number>(1)
   const [nextCard, setNextCard] = useState<number>(0)
 
-  const animPosition1 = useRef(new Animated.Value(CARD_STACK * 1)).current;
-  const animPosition2 = useRef(new Animated.Value(CARD_STACK * 2)).current;
-  const animPosition3 = useRef(new Animated.Value(CARD_STACK * 3)).current;
-  const animOpacity1 = useRef(new Animated.Value(.7)).current;
-  const animOpacity2 = useRef(new Animated.Value(.5)).current;
-  const animOpacity3 = useRef(new Animated.Value(0)).current;
-  const animWidth1 = useRef(new Animated.Value(WIDTH * .9)).current;
-  const animWidth2 = useRef(new Animated.Value(WIDTH * .8)).current;
-  const animWidth3 = useRef(new Animated.Value(WIDTH * .7)).current;
+  const animArray = []
+
+  for (let i = 0; i < animValues.length; i++) {
+    animArray.push({
+      position: useRef(new Animated.Value(animValues[i].position)).current,
+      opacity: useRef(new Animated.Value(animValues[i].opacity)).current,
+      width: useRef(new Animated.Value(animValues[i].width)).current
+    })
+  }
+
 
   useEffect(() => {
-    (async () => setMovieList([movieList, ...(await getMovies(pageNumber)).results]))()
+    (async () => setMovieList([...movieList, ...(await getMovies(pageNumber)).results]))()
   }, [pageNumber])
 
   const updateIndex = (i: number) => {
-    if (nextCard + 4 < movieList.length) {
-      setNextCard(nextCard + 1)
-    } else {
+    setNextCard(nextCard + 1)
+    if (nextCard + animArray.length + 3 > movieList.length) {
       setPageNumber(pageNumber + 1)
-      // setNextCard(0)
     }
   }
 
   const startAnim = (callback = () => { }) => {
-    Animated.parallel([
-      Animated.timing(animPosition1, { toValue: CARD_STACK * 0, duration: ANIM_DURATION, useNativeDriver: false }),
-      Animated.timing(animOpacity1, { toValue: 1, duration: ANIM_DURATION, useNativeDriver: false }),
 
-      Animated.timing(animPosition2, { toValue: CARD_STACK * 1, duration: ANIM_DURATION, useNativeDriver: false }),
-      Animated.timing(animOpacity2, { toValue: .7, duration: ANIM_DURATION, useNativeDriver: false }),
+    const cards = [
+      Animated.timing(animArray[0].position, { toValue: 0, duration: ANIM_DURATION, useNativeDriver: false }),
+      Animated.timing(animArray[0].opacity, { toValue: 1, duration: ANIM_DURATION, useNativeDriver: false }),
+      Animated.timing(animArray[0].width, { toValue: 1, duration: ANIM_DURATION, useNativeDriver: false }),
+    ]
+    for (let i = 1; i < animValues.length; i++) {
+      cards.push(...[
+        Animated.timing(animArray[i].position, { toValue: animValues[i - 1].position, duration: ANIM_DURATION, useNativeDriver: false }),
+        Animated.timing(animArray[i].opacity, { toValue: animValues[i - 1].opacity, duration: ANIM_DURATION, useNativeDriver: false }),
+        Animated.timing(animArray[i].width, { toValue: animValues[i - 1].width, duration: ANIM_DURATION, useNativeDriver: false })
+      ])
+    }
 
-      Animated.timing(animPosition3, { toValue: CARD_STACK * 2, duration: ANIM_DURATION, useNativeDriver: false }),
-      Animated.timing(animOpacity3, { toValue: .5, duration: ANIM_DURATION, useNativeDriver: false })
-    ], { stopTogether: true }).start(() => callback())
+    Animated.parallel(cards, { stopTogether: true }).start(() => callback())
 
   }
 
   const flowNextCard = () => {
-    console.log("start next card")
     startAnim(() => {
-      animOpacity1.setValue(.7)
-      animOpacity2.setValue(.5)
-      animOpacity3.setValue(0)
-      animPosition1.setValue(CARD_STACK * 1)
-      animPosition2.setValue(CARD_STACK * 2)
-      animPosition3.setValue(CARD_STACK * 3)
-      animWidth1.setValue(WIDTH * .9)
-      animWidth2.setValue(WIDTH * .8)
-      animWidth3.setValue(WIDTH * .7)
+      for (let i = 0; i < animValues.length; i++) {
+        animArray[i].opacity.setValue(animValues[i].opacity)
+        animArray[i].position.setValue(animValues[i].position)
+        animArray[i].width.setValue(animValues[i].width)
+      }
     })
   }
 
-  if (movieList.length == 0) {
+  if (movieList == undefined || movieList.length == 0) {
     return <FontText fontSize={23} font={"Roboto-Bold"}>loading</FontText>
   }
 
   return (
     <View>
-      <Animated.View style={{ marginTop: animPosition3, position: "absolute", alignSelf: "center", width: animWidth3 }}>
-        <View style={[styles.card]}>
-          <View style={[styles.cardImg, styles.blackBack]} />
-          <Animated.Image style={[styles.cardImg, { opacity: animOpacity3 }]} source={{ uri: baseUrl500 + movieList[nextCard + 3].poster_path }} />
-        </View>
-      </Animated.View>
-
-      <Animated.View style={{ marginTop: animPosition2, position: "absolute", alignSelf: "center", width: animWidth2 }}>
-        <View style={[styles.card]}>
-          <View style={[styles.cardImg, styles.blackBack]} />
-          <Animated.Image style={[styles.cardImg, { opacity: animOpacity2 }]} source={{ uri: baseUrl500 + movieList[nextCard + 2].poster_path }} />
-        </View>
-      </Animated.View>
-
-      <Animated.View style={{ marginTop: animPosition1, position: "absolute", alignSelf: "center", width: animWidth1 }}>
-        <View style={[styles.card]}>
-          <View style={[styles.cardImg, styles.blackBack]} />
-          <Animated.Image style={[styles.cardImg, { opacity: animOpacity1 }]} source={{ uri: baseUrl500 + movieList[nextCard + 1].poster_path }} />
-        </View>
-      </Animated.View>
+      {animArray.map((anim, index) =>
+        <Animated.View key={index}
+          style={{ marginTop: animArray[animArray.length - index - 1].position, position: "absolute", alignSelf: "center" }}>
+          <View style={[styles.card]}>
+            <View style={[styles.cardImg, styles.blackBack]} />
+            <Animated.Image
+              style={[styles.cardImg, { opacity: animArray[animArray.length - index - 1].opacity, transform: [{ scaleX: animArray[animArray.length - index - 1].width }] }]}
+              source={{ uri: baseUrl500 + movieList[nextCard + animArray.length - index].poster_path }} />
+          </View>
+        </Animated.View>
+      )}
 
       <Card movie={movieList[nextCard]} updateIndex={updateIndex} getNextCard={flowNextCard} />
+
+      <ExtraInfo movie={movieList[nextCard]} />
     </View>
   )
 }
@@ -211,5 +235,37 @@ const styles = StyleSheet.create({
   blackBack: {
     position: "absolute",
     backgroundColor: Colors.background
-  }
+  },
+  extraInfoContainer: {
+    marginTop: 40,
+    width: WIDTH * .9,
+    alignSelf:"center",
+  },
+  infoHolder: {
+    
+  },
+  infoRow: {
+    justifyContent:"space-between",
+    flexDirection: "row"
+  },
+  info: {
+    justifyContent:"center",
+    alignItems:"center",
+    width: WIDTH / 2 * .8,
+    height: 40,
+    borderRadius:10,
+    backgroundColor: Colors.darkLight
+  },
+  movieRating: {
+    flexDirection: "row",
+  },
+  voteAverage: {
+    color: Colors.textColor,
+    fontSize: 20,
+    fontWeight:"bold"
+  },
+  topStar: {
+    marginTop: 3,
+    marginRight: 5,
+  },
 })
